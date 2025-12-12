7b395802681a62746cf4f9642be183a5e8f5306b
Amr Ashraf Saad <116368529+amrashraf20201@users.noreply.github.com>
2025-12-13 01:40:42 +0200

    Delete Graduation-Project-master directory

--- Graduation-Project-master/ExplurerHub/src/main/java/com/example/explurerhub/Repository/FavouriteMusimRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.explurerhub.Repository;
-
-import com.example.explurerhub.Model.CairoMusiums;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FavouriteMusimRepo extends JpaRepository<CairoMusiums,Long> {
-}
--- Graduation-Project-master/ExplurerHub/src/main/java/com/example/explurerhub/Repository/FavouriteRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.explurerhub.Repository;
-
-import com.example.explurerhub.Model.CairoMosques;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FavouriteRepo extends JpaRepository<CairoMosques,Long> {
-}
